9a01ec3bc7ef2f22af296189050f9e51f52be243
Alan George <89032126+Algeo3603@users.noreply.github.com>
2023-06-25 15:13:46 +0530

    Create boilerplate.cpp

---
+++ boilerplate.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+void testcase() {
+
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int tc = 1;
+    // cin >> tc;
+    while (tc--) testcase();
+    return 0;
+}
