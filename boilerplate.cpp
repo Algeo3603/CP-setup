90a5f2798841b16ccbcf06d10c089278b7be8065
Alan George <89032126+Algeo3603@users.noreply.github.com>
2023-07-11 14:36:39 +0530

    Update boilerplate.cpp

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -8,11 +8,7 @@ void testcase() {
 
 int32_t main() {
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-
+    
     int tc = 1;
     // cin >> tc;
     while (tc--) testcase();
