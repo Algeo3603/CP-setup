07b2c6fc8b9343b5accebf511f3152d1358fede8
Alan George <89032126+Algeo3603@users.noreply.github.com>
2023-07-18 22:24:58 +0530

    Update matrix_exponentiation.cpp

--- matrix_exponentiation.cpp
+++ matrix_exponentiation.cpp
@@ -1,4 +1,4 @@
-vector<vector<int>> mat_mul(vector<vector<int>> a, vector<vector<int>> b) {
+vector<vector<int>> mat_mul(vector<vector<int>> &a, vector<vector<int>> &b) {
     vector<vector<int>> res(a.size(), vector<int>(b[0].size()));
     for (int i = 0; i < a.size(); i++) {
         for (int j = 0; j < b[0].size(); j++) {
@@ -11,10 +11,9 @@ vector<vector<int>> mat_mul(vector<vector<int>> a, vector<vector<int>> b) {
     return res;
 }
 
-vector<vector<int>> mat_expo(vector<vector<int>> a, int b) {
+vector<vector<int>> mat_expo(vector<vector<int>> &a, int b) {
     vector<vector<int>> res(a.size(), vector<int>(a.size()));
     for (int i = 0; i < a.size(); i++) res[i][i] = 1;
-
     while (b) {
         if (b & 1) {
             res = mat_mul(res, a);
@@ -22,9 +21,9 @@ vector<vector<int>> mat_expo(vector<vector<int>> a, int b) {
         a = mat_mul(a, a);
         b >>= 1;
     }
-
     return res;
 }
 
-// vector<vector<int>> ans = mat_mul(states, mat_expo(transition, power));
 // transition[i][j] = how many times does prev state i contibute to next state j
+// transition = mat_expo(transition, power);
+// states = mat_mul(states, transition);
