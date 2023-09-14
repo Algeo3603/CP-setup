2b34d9fb89afd4068f21160b3bcd26b17cef1d23
Alan George <89032126+Algeo3603@users.noreply.github.com>
2023-09-14 22:19:06 +0530

    Create binary_lifting_LCA.cpp

---
+++ binary_lifting_LCA.cpp
@@ -0,0 +1,47 @@
+const int MAX_N = ?, LOG = log2(MAX_N) + 1;
+int up[MAX_N][LOG];
+int depth[MAX_N];
+vector<int> children[MAX_N];
+
+void dfs(int a) {
+    for(int b : children[a]) {
+        depth[b] = depth[a] + 1;
+        up[b][0] = a; // a is parent of b
+        for(int j = 1; j < LOG; j++) {
+            up[b][j] = up[up[b][j-1]][j-1];
+        }
+        dfs(b);
+    }
+}
+
+int getLCA(int a ,int b) {
+    // ensure a is deeper than b
+    if (depth[a] < depth[b]) {
+        swap(a, b);
+    }
+
+    // lift a to the same depth as b
+    int k = depth[a] - depth[b];
+    for (int j = LOG - 1; j >= 0; j--) {
+        if (k & (1 << j)) {
+            a = up[a][j];
+        }
+    }
+
+    // lift both a and b as high as possible without them having the same ancestor
+    for (int j = LOG - 1; j >= 0; j--) {
+        if (up[a][j] != up[b][j]) {
+            a = up[a][j];
+            b = up[b][j];
+        }
+    }
+
+    if (a == b) {
+        return a;
+    } else {
+        return up[a][0];
+    }
+}
+
+// call dfs which computes depth and ancestors
+dfs(0);
